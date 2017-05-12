0d7e24cb726f5d4bb063f34ee6af899e513c943b
Dan Schaefer <dan@schaeferzone.net>
2017-05-12 12:44:50 -0400

    add xinitrc file and update xrandr_laptop

--- roles/term/files/bin/xrandr_laptop.sh
+++ roles/term/files/bin/xrandr_laptop.sh
@@ -2,6 +2,33 @@
 
 set -euo pipefail
 
-xrandr --output DP-1 --auto
-xrandr --output LVDS-1 --auto
-xrandr --output DP-1 --left-of LVDS-1
+xrandr_out=`xrandr`
+echo $xrandr_out | grep 'eDP1 connected' 2>&1 >/dev/null
+if [ $? -eq 0 ]; then
+    LAPTOP_DISPLAY=eDP1
+else
+    LAPTOP_DISPLAY=eDP2
+fi
+
+echo $xrandr_out | grep 'DP1 connected' 2>&1 >/dev/null
+if [ $? -eq 0 ]; then
+    VGA_CONF="--output DP1 --auto --right-of $LAPTOP_DISPLAY"
+else
+    VGA_CONF='--output P1 --off'
+fi
+
+echo $xrandr_out | grep 'VGA1 connected' 2>&1 >/dev/null
+if [ $? -eq 0 ]; then
+    VGA_CONF="--output VGA1 --auto --right-of $LAPTOP_DISPLAY"
+else
+    VGA_CONF='--output VGA1 --off'
+fi
+
+echo $xrandr_out | grep 'HDMI1 connected' 2>&1 >/dev/null
+if [ $? -eq 0 ]; then
+    HDMI_CONF="--output HDMI1 --auto --right-of $LAPTOP_DISPLAY"
+else
+    HDMI_CONF='--output HDMI1 --off'
+fi
+
+xrandr --output $LAPTOP_DISPLAY --auto $VGA_CONF $HDMI_CONF &
