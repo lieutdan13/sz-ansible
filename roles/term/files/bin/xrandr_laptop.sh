e67ce1047d746068d3753f207caaee28003b59c9
Dan Schaefer <dan@schaeferzone.net>
2017-10-17 13:06:15 -0400

    add xrandr script changes

--- roles/term/files/bin/xrandr_laptop.sh
+++ roles/term/files/bin/xrandr_laptop.sh
@@ -3,33 +3,36 @@
 # TODO: pipe all greps to true and compare to an empty string
 #set -euo pipefail
 
+EXTERNAL_OUT=""
+
 xrandr_out=`xrandr`
-echo $xrandr_out | grep 'eDP1 connected' 2>&1 >/dev/null
-if [ $? -eq 0 ]; then
-    LAPTOP_DISPLAY=eDP1
-else
-    LAPTOP_DISPLAY=eDP2
-fi
 
-echo $xrandr_out | grep 'DP1 connected' 2>&1 >/dev/null
-if [ $? -eq 0 ]; then
-    VGA_CONF="--output DP1 --auto --right-of $LAPTOP_DISPLAY"
-else
-    VGA_CONF='--output P1 --off'
-fi
+PRIMARY=`echo "$xrandr_out" | grep 'primary' | awk '{ print $1 }'`
+PRIMARY_RESOLUTION="1920x1080"
+echo "Laptop Display = $PRIMARY"
+echo "${PRIMARY} Resolution = $PRIMARY_RESOLUTION"
+echo
 
-echo $xrandr_out | grep 'VGA1 connected' 2>&1 >/dev/null
-if [ $? -eq 0 ]; then
-    VGA_CONF="--output VGA1 --auto --right-of $LAPTOP_DISPLAY"
-else
-    VGA_CONF='--output VGA1 --off'
-fi
+RIGHT_OF=$PRIMARY
+EXTERNALS=`echo "$xrandr_out" | grep -v 'primary' | grep 'connected' | awk '{ print $1 }'`
+for EXTERNAL_DEVICE in $EXTERNALS; do
+    echo "External Display: $EXTERNAL_DEVICE"
+    echo "$xrandr_out" | grep -E "^$EXTERNAL_DEVICE connected" > /dev/null
+    EXTERNAL_CONNECTED=$?
 
-echo $xrandr_out | grep 'HDMI1 connected' 2>&1 >/dev/null
-if [ $? -eq 0 ]; then
-    HDMI_CONF="--output HDMI1 --auto --right-of $LAPTOP_DISPLAY"
-else
-    HDMI_CONF='--output HDMI1 --off'
-fi
+    if [ $EXTERNAL_CONNECTED -eq 0 ]; then
+        echo "${EXTERNAL_DEVICE} Connected: yes"
+        echo "${EXTERNAL_DEVICE} Resolution: auto"
+        echo "${EXTERNAL_DEVICE} Position: right of $RIGHT_OF"
+        EXTERNAL_OUT="${EXTERNAL_OUT} --output $EXTERNAL_DEVICE --auto --right-of $RIGHT_OF"
+        RIGHT_OF=$EXTERNAL_DEVICE
+    else
+        echo "${EXTERNAL_DEVICE} Connected: no"
+        EXTERNAL_OUT="${EXTERNAL_OUT} --output $EXTERNAL_DEVICE --off"
+    fi
+    echo
+done
 
-xrandr --output $LAPTOP_DISPLAY --auto $VGA_CONF $HDMI_CONF &
+echo
+echo "xrandr --output $PRIMARY --mode $PRIMARY_RESOLUTION $EXTERNAL_OUT &"
+xrandr --output $PRIMARY --mode $PRIMARY_RESOLUTION $EXTERNAL_OUT &
